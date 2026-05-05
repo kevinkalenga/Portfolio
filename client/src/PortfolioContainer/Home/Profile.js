d3bc01342bb62ef560951a0525503484f1e1df24
kevin <kevinkalenga10@gmail.com>
2026-05-06 00:44:23 +0200

    Profile Component1

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+       <div className='profile-parent'>
+          <div className='profile-details'>
+             <div className='colz'>
+                <a href="https://www.facebook.com/king.kevinkonvict">
+                    <i className='fa fa-facebook-square'></i>
+                </a>
+               
+                <a href="#">
+                    <i className='fa fa-instagram'></i>
+                </a>
+                <a href="https://github.com/kevinkalenga">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+                <a href="#">
+                    <i className='fa fa-twitter'></i>
+                </a>
+               
+             </div>
+          </div>
+       </div>
+    </div>
+  )
+}
